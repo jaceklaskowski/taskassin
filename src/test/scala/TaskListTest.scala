023d7ed286d96ed7a65392d00d131a8a71942f9f
Jacek Laskowski <jacek@japila.pl>
2013-06-15 22:46:42 +0200

    Instantiating fixture traits from FunSpec

--- src/test/scala/TaskListTest.scala
+++ src/test/scala/TaskListTest.scala
@@ -8,27 +8,21 @@ import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
-class TaskTest extends Spec with ShouldMatchers with BeforeAndAfter {
+class TaskTest extends Spec with ShouldMatchers {
 
-  var ts: List[Task] = _
-  val t1 = new Task("t1", DateTime.now)
-
-  before {
+  trait Fixture {
+    val t1 = new Task("t1", DateTime.now)
     val t2 = new Task("t2", DateTime.now + 2.minutes)
-
-    /* http://mkaz.com/solog/how-to-sort-collections-in-scala */
-    /* sort by due date */
     def compByDueDate(t1: Task, t2: Task) = t1.due < t2.due
-
-    ts = List(t2, t1) sortWith compByDueDate
+    val ts = List(t2, t1) sortWith compByDueDate
   }
 
   describe("List of three tasks (with 5 minutes in between)") {
     it("should pop tasks by due order") {
-      ts.head === t1
+      new Fixture {
+        assert(ts.head === t1)
+      }
     }
-    it("should return the tasks for today") {
-      pending
-    }
+    it("should return the tasks for today") (pending)
   }
 }
