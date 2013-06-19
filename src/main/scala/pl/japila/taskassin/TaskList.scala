2ae19be8622d1689fb4749355cdaaea2dc1d5eea
Jacek Laskowski <jacek@japila.pl>
2013-06-19 16:07:37 +0200

    collectTasks function to collect tasks for given hours

--- src/main/scala/pl/japila/taskassin/TaskList.scala
+++ src/main/scala/pl/japila/taskassin/TaskList.scala
@@ -1,5 +1,20 @@
 package pl.japila.taskassin
 
 import org.joda.time.DateTime
+import annotation.tailrec
 
-case class Task(descr: String, due: DateTime)
+case class Task(label: String, due: DateTime, timeSpanInHours: Int = 6)
+
+object TaskUtils {
+
+  def collectTasks(ts: List[Task], hours: Int): List[Task] = {
+    @tailrec
+    def collectTasks(ts: List[Task], accTs: List[Task], remHours: Int): List[Task] = 
+      ts match {
+        case t :: tail if (remHours >= 0 && remHours - t.timeSpanInHours >= 0) =>
+          collectTasks(tail, t :: accTs, remHours - t.timeSpanInHours)
+        case _ => accTs reverse
+      }
+    collectTasks(ts, Nil, hours)
+  }
+}
--- src/test/scala/TaskListTest.scala
+++ src/test/scala/TaskListTest.scala
@@ -7,22 +7,33 @@ import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 
+import pl.japila.taskassin.TaskUtils.collectTasks
+
 @RunWith(classOf[JUnitRunner])
 class TaskTest extends Spec with ShouldMatchers {
 
   trait Fixture {
-    val t1 = new Task("t1", DateTime.now)
-    val t2 = new Task("t2", DateTime.now + 2.minutes)
+    val now = DateTime.now
+    val t1 = new Task("t1", now, 10)
+    val t2 = new Task("t2", now + 2.hours)
+    val t3 = new Task("t3", now - 2.hours)
+
     def compByDueDate(t1: Task, t2: Task) = t1.due < t2.due
-    val ts = List(t2, t1) sortWith compByDueDate
+    val ts = List(t1, t2, t3) sortWith compByDueDate
   }
 
-  describe("List of three tasks (with 5 minutes in between)") {
+  describe("List of three tasks (with 2 hours in between)") {
     it("should pop tasks by due order") {
       new Fixture {
-        assert(ts.head === t1)
+        assert(ts.head === t3)
       }
     }
-    it("should return the tasks for today") (pending)
+    it("should return the next task for today") {
+      new Fixture {
+        val hoursPerDay = 6
+        val todayTs = collectTasks(ts, hoursPerDay)
+        assert(todayTs === List(t3))
+      }
+    }
   }
 }
