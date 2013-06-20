0882b91ae7d1ba2ed8d47471b263f5e07833b6df
Jacek Laskowski <jacek@japila.pl>
2013-06-20 21:24:24 +0200

    recurring task support

--- src/main/scala/pl/japila/taskassin/TaskList.scala
+++ src/main/scala/pl/japila/taskassin/TaskList.scala
@@ -3,18 +3,25 @@ package pl.japila.taskassin
 import org.joda.time.DateTime
 import annotation.tailrec
 
-case class Task(label: String, due: DateTime, timeSpanInHours: Int = 6)
+case class Task(label: String, due: DateTime, timeUnits: Int = 6,
+  recurring: Boolean = false, recurTimes: Int = 0, intervalUnits: Int = 0)
 
 object TaskUtils {
 
   def collectTasks(ts: List[Task], hours: Int): List[Task] = {
     @tailrec
-    def collectTasks(ts: List[Task], accTs: List[Task], remHours: Int): List[Task] = 
+    def collectTasks(ts: List[Task], accTs: List[Task], remHours: Int): List[Task] = {
       ts match {
-        case t :: tail if (remHours >= 0 && remHours - t.timeSpanInHours >= 0) =>
-          collectTasks(tail, t :: accTs, remHours - t.timeSpanInHours)
+        case t :: tail if (remHours >= 0 && remHours - t.timeUnits >= 0) =>
+          if (t.recurring) {
+            val tt = t.copy(recurTimes = t.recurTimes - 1)
+            val tss = tt :: tail
+            collectTasks(tss, t :: accTs, remHours - t.timeUnits - t.intervalUnits)
+          } else
+            collectTasks(tail, t :: accTs, remHours - t.timeUnits)
         case _ => accTs reverse
       }
+    }
     collectTasks(ts, Nil, hours)
   }
 }
--- src/test/scala/TaskListTest.scala
+++ src/test/scala/TaskListTest.scala
@@ -14,12 +14,18 @@ class TaskTest extends Spec with ShouldMatchers {
 
   trait Fixture {
     val now = DateTime.now
-    val t1 = new Task("t1", now, 10)
-    val t2 = new Task("t2", now + 2.hours)
-    val t3 = new Task("t3", now - 2.hours)
+    val t1 = Task("t1", now, 10)
+    val t2 = Task("t2", now + 2.hours)
+    val t3 = Task("t3", now - 2.hours)
+    val t4r = Task(label = "t4r",
+                   due = now - 2.hours, 
+                   timeUnits = 1,
+                   recurring = true,
+                   recurTimes = 2,
+                   intervalUnits = 1)
 
     def compByDueDate(t1: Task, t2: Task) = t1.due < t2.due
-    val ts = List(t1, t2, t3) sortWith compByDueDate
+    val ts = List(t1, t2, t3, t4r) sortWith compByDueDate
   }
 
   describe("List of three tasks (with 2 hours in between)") {
@@ -28,11 +34,25 @@ class TaskTest extends Spec with ShouldMatchers {
         assert(ts.head === t3)
       }
     }
-    it("should return the next task for today") {
+    it("should return the tasks for today") {
       new Fixture {
         val hoursPerDay = 6
         val todayTs = collectTasks(ts, hoursPerDay)
         assert(todayTs === List(t3))
+      }
+    }
+  }
+
+  describe("List of 3 tasks (with 1 recurring task)") {
+    it("should return recurring task twice") {
+      new Fixture {
+        val timeUnits = 9
+        val todayTs = collectTasks(ts, timeUnits)
+        assert(todayTs.size === 3)
+        assert(todayTs.head === t3)
+        assert(todayTs.tail.head === t4r)
+        val t4rCopied = t4r.copy(recurTimes = t4r.recurTimes - 1)
+        assert(todayTs.tail.tail.head === t4rCopied)
       }
     }
   }
